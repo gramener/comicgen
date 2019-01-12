980d823f88e59e275a8c7a53fc4cf95a321ce779
Richie Lionell <richie.lionell@gramener.com>
2019-01-12 20:55:22 +0530

    position Dee's features

--- js/deedey.js
+++ js/deedey.js
@@ -1,6 +1,10 @@
+var body_xpos, body_ypos;
+body_xpos = 30.5
+body_ypos = 77
 $('script[type="text/html"]').template({
-  files: ['files/Dee/Body/Dee checking mobile.svg',
-          'files/Dee/Mouth/confused.svg',
-          'files/Dee/Eyes/Eyes - Angry.svg',
-          'files/Dee/Hair/Hair.svg']
+  files: [['files/Dee/Body/Dee checking mobile.svg',0, 0],
+  		  ['files/Dee/Face.svg',29.5, 11],
+          ['files/Dee/Mouth/confused.svg', 80.5, 92],
+          ['files/Dee/Eyes/Eyes - Angry.svg', 63.5, 58],
+          ['files/Dee/Hair/Hair.svg', 29.5, 3]]
 })
