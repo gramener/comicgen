31893a3ad36f3e29b82b11b74282574b1ed853a4
S Anand <root.node@gmail.com>
2019-03-06 14:01:10 +0530

    WIP

--- js/deedey.js
+++ js/deedey.js
@@ -1,10 +1,10 @@
-var body_xpos, body_ypos;
-body_xpos = 30.5
-body_ypos = 77
-$('script[type="text/html"]').template({
-  files: [['files/Dee/Body/Dee checking mobile.svg',0, 0],
-  		  ['files/Dee/Face.svg',29.5, 11],
-          ['files/Dee/Mouth/confused.svg', 80.5, 92],
-          ['files/Dee/Eyes/Eyes - Angry.svg', 63.5, 58],
-          ['files/Dee/Hair/Hair.svg', 29.5, 3]]
+$.getJSON('filelist')
+  .done(function(files) {
+    $('script.checklist').template({files: files})
+  })
+
+$('body').on('change', 'input.file', function() {
+  $('script.comic').template({
+    files: $('input:checked').map(function (v) { return 'files' + this.name }).get()
+  })
 })
