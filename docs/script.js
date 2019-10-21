d624112efa6d8825c63026623b6c90fd6d2a691d
Kriti Rohilla <kriti.rohilla@gramener.org>
2019-10-21 11:07:17 +0530

    code refactor

--- docs/script.js
+++ docs/script.js
@@ -211,33 +211,31 @@ function emotionposecombinations(basestr, emotionarr, posarr){
 $.getJSON('files.json')
   .done(function (data) {
     var node = data
-    var namearr = Object.keys(node)
+    var anglearr = {
+      'aryan': [],
+      'dee': ['straight', 'side', 'sitting'],
+      'dey': ['straight', 'side', 'sitting'],
+      'priya': ['straight', 'sitting'],
+      'ringo': ['straight', 'sitting'],
+      'zoe': []
+    }
+    var namearr = Object.keys(anglearr)
 
     namearr.forEach(function(character_name){
       var q = g1.url.parse('').update({name: character_name}).toString()
 
-      if (['aryan', 'zoe'].includes(character_name)){  
+      if (anglearr[character_name].length > 0){
+        anglearr[character_name].forEach(function(a){
+          q = g1.url.parse(q).update({angle: a}).toString()
+          var allemotions = node[character_name][a]['emotion']
+          var allposes = node[character_name][a]['pose']
+          emotionposecombinations(q, allemotions, allposes)
+        })
+      }
+      else {
         var allemotions = node[character_name]['emotion']
         var allposes = node[character_name]['pose']
         emotionposecombinations(q, allemotions, allposes)
-      }
-      else if (['dee', 'dey'].includes(character_name)){
-        var angles = ['straight', 'side', 'sitting']
-        angles.forEach(function(a){
-          q = g1.url.parse(q).update({angle: a}).toString()
-          var allemotions = node[character_name][a]['emotion']
-          var allposes = node[character_name][a]['pose']
-          emotionposecombinations(q, allemotions, allposes)
-        })
-      }
-      else if (['priya', 'ringo'].includes(character_name)){
-        var angles = ['straight', 'sitting']
-        angles.forEach(function(a){
-          q = g1.url.parse(q).update({angle: a}).toString()
-          var allemotions = node[character_name][a]['emotion']
-          var allposes = node[character_name][a]['pose']
-          emotionposecombinations(q, allemotions, allposes)
-        })
       }
     })
   })
