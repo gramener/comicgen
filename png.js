7212f0bcbf9b2ec8d171ef58be07700b59b66a54
Tejesh <tejesh.p@gramener.com>
2019-12-30 12:39:48 +0530

    remove svg-png-converter as dependency

--- png.js
+++ png.js
@@ -1,18 +1,42 @@
-var convertor = require('svg-png-converter')
-var glob = require('glob')
 var fs = require('fs')
+var glob = require('glob')
+var jsbase64 = require('js-base64')
+var fabric = require('fabric').fabric
 
 glob('./svg/**/*.svg', {}, function (er, files) {
   files.forEach(function (file) {
     fs.mkdir(file.replace(/\/svg\//, '/png/').replace(/\/[^/]+$/, ''), { recursive: true }, function (error) {
       if (error) throw error
-      convertor.svg2png({
-        input: fs.readFileSync(file),
-        encoding: 'buffer',
-        format: 'png',
-      }).then(function (outputBuffer) {
+      svg2png(fs.readFileSync(file), function(outputBuffer) {
         fs.writeFileSync(file.replace(/\/svg\//, '/png/').replace(/\.svg/, '.png'), outputBuffer)
       })
     })
   })
 })
+
+function svg2png(buffer, callback) {
+  var canvas = new fabric.Canvas('c')
+  var url = dataToUrl(buffer.toString(), 'svg')
+  fabric.Image.fromURL(url, (img) => {
+    canvas.add(img)
+    const s = canvas.toDataURL({ height: img.height, width: img.width, format: 'png' })
+    var buffer = Buffer.from(urlToBase64(s), 'base64')
+    callback(buffer)
+  })
+}
+
+function dataToUrl(data, mimeType, fileName) {
+  return base64ToUrl(dataToBase64(data), mimeType, fileName)
+}
+
+function urlToBase64(s) {
+  return s.substring(s.indexOf(';base64,') + ';base64,'.length)
+}
+
+function base64ToUrl(base64, mimeType, fileName) {
+  return `data:${mimeType}${fileName ? `;name=${fileName}` : ''};base64,${base64}`
+}
+
+function dataToBase64(data) {
+  return jsbase64.Base64.encode(data)
+}
