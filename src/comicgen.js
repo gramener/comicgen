168dd1105eb56944a254be9403b676de274c4b6a
Tejesh P <tejesh95.p@gmail.com>
2020-03-27 11:50:40 +0530

    merge

--- src/comicgen.js
+++ src/comicgen.js
@@ -1,6 +1,12 @@
 /* eslint-disable no-console */
+import files from './files.json'
 import { version } from '../package.json'
 import { defaults, namemap, formats } from './characters.json'
+
+var unique_id_counter = 0
+function generate_unique_id(attr_key) {
+  return 'gcontainer-' + attr_key + unique_id_counter++
+}
 
 export default function comicgen(selector, options) {
   // Selector can be false-y, string selector or DOM node. Defaults to ".comicgen"
@@ -37,22 +43,108 @@ export default function comicgen(selector, options) {
       `<g transform="scale(${attrs.scale})${mirror}">`
     ]
 
+    var parametricUrls = []
     // Loop through all attributes (e.g. emotion=, pose=, body=, etc)
     // If the attribute is in format.file, there's an image for it. Add it.
     for (var attr in attrs) {
       if (attr in format.files) {
         var row = format.files[attr]
         // Substitute any $variable with the corresponding attribute value
-        var img = row.file.replace(/\$([a-z]*)/g, function (match, group) { return attrs[group] })
-        svg.push(`<image width="${row.width}" height="${row.height}" transform="translate(${row.x},${row.y})" xlink:href="${comicgen.base}${attrs.ext}/${img}.${attrs.ext}"/>`)
+        if (row.param) {
+          var sliderVal = attrs[attr]
+
+          files[attrs['name']][attr].forEach(function(filename) {
+            var id = generate_unique_id(attr)
+            svg.push(`<g id="${id}"></g>`)
+
+            var img = row.file.replace(/\$([a-z]*)/g, function (match, group) { return group == row.param ? filename : attrs[group] })
+            parametricUrls.push({
+              getRequest: $.get(`${comicgen.base}svg/${img}.svg`, undefined, undefined, 'text'),
+              id: id,
+              sliderVal: sliderVal
+            })
+          })
+        }
+        else {
+          var img = row.file.replace(/\$([a-z]*)/g, function (match, group) { return attrs[group] })
+          svg.push(`<image width="${row.width}" height="${row.height}" transform="translate(${row.x},${row.y})" xlink:href="${comicgen.base}${attrs.ext}/${img}.${attrs.ext}"/>`)
+        }
       }
     }
+
+    $.when(...parametricUrls.map(function(d) {return d.getRequest}))
+      .done(function (...svg_responses) {
+        // svg_responses length is always even. Each consecutive pair is one body part.
+        for (var i = 0; i < parametricUrls.length; i = i + 2) {
+          $('#'+parametricUrls[i]['id']).append(svg_responses[i][0])
+          $('#'+parametricUrls[i]['id']).append(`<template>${svg_responses[i][0]}</template>`)
+          $('#'+parametricUrls[i]['id']).append(`<template>${svg_responses[i + 1][0]}</template>`)
+          create_parametric_svg(node, parametricUrls[i])
+        }
+      })
 
     // Add the SVG footer
     svg.push('</g></svg>')
     node.innerHTML = svg.join('')
 
     // TODO: trigger an event
+  })
+}
+
+
+function create_parametric_svg(node, param) {
+  var original_id = node.querySelector(`#${param.id} svg g`).id
+  var all_character_tags = node.querySelectorAll('#'+original_id + ' *')
+
+  function interpolate_path_d(attr, start_element, end_element) {
+    return flubber.interpolate(start_element.getAttribute(attr), end_element.getAttribute(attr), { maxSegmentLength: 0.2 })(param.sliderVal)
+  }
+
+  function interpolate_shape_attr(attr, start_element, end_element) {
+    return ($(end_element).attr(attr) - $(start_element).attr(attr)) * param.sliderVal + +$(start_element).attr(attr)
+  }
+
+  function interpolate_generic_attr(attr, start_element, end_element) {
+    return d3.interpolate($(start_element).attr(attr), $(end_element).attr(attr))(param.sliderVal)
+  }
+
+  var interpolatorMap = {
+    'transform': interpolate_generic_attr,
+    'fill': interpolate_generic_attr,
+    'stroke': interpolate_generic_attr,
+    'stroke-width': interpolate_generic_attr,
+    'd': interpolate_path_d,
+    'cx': interpolate_shape_attr,
+    'cy': interpolate_shape_attr,
+    'r': interpolate_shape_attr,
+    'rx': interpolate_shape_attr,
+    'ry': interpolate_shape_attr,
+  }
+  var elementTypes = {
+    path: {
+      attributes: ['d']
+    },
+    circle: {
+      attributes: ['cx', 'cy', 'r']
+    },
+    ellipse: {
+      attributes: ['cx', 'cy', 'rx', 'ry']
+    }
+  }
+
+  all_character_tags.forEach(function (character_tag) {
+    if(!character_tag.id) return
+
+    var real_element = node.querySelector(`#${param.id} #${character_tag.id}`)
+    var start_element = node.querySelector(`#${param.id} template:nth-child(2)`).content
+      .cloneNode(true).querySelector(`#${character_tag.id}`)
+    var end_element = node.querySelector(`#${param.id} template:nth-child(3)`).content
+      .cloneNode(true).querySelector(`#${character_tag.id}`)
+
+    var elementType = elementTypes[start_element.tagName]
+    elementType && elementType['attributes'].concat(['transform', 'fill', 'stroke', 'stroke-width'])
+      .forEach(d => real_element.getAttribute(d) &&
+                    real_element.setAttribute(d, interpolatorMap[d](d, start_element, end_element)))
   })
 }
 
