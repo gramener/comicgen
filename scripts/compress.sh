bc866676d800d5dbd87b7f97d6d1a01e012b3cdf
S Anand <root.node@gmail.com>
2020-05-04 16:47:34 +0530

    BUG: Don't compress continous chars. Segregate sliders & inputs. Fix #70

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -3,9 +3,13 @@
 # Loop through all SVG files under the svg/ folder
 for svg in $(find svg -name "*.svg")
 do
-  echo "$svg"
   # Compress PNG files
+  if ([[ $svg == svg/chini* ]] || [[ $svg == svg/panda* ]] || [[ $svg == svg/zoozoo* ]])
+  then
+    continue
+  fi
+  echo "$svg"
   pngquant --force --ext .png "${svg//svg/png}"
-  # Compress the SVG. Enabling convertPathData may fail fabricjs to load SVGs.
-  node_modules/.bin/svgo -q "$svg" --multipass --disable="convertPathData"
+  # # Compress the SVG. Enabling convertPathData may fail fabricjs to load SVGs.
+  node_modules/.bin/svgo -q --multipass "$svg"
 done
