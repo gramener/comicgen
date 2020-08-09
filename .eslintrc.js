38ebacb740287519019431e5dc8472ba0a523a67
S Anand <root.node@gmail.com>
2020-08-09 16:42:07 +0530

    ENH: Add nuovo versions of Dee, Dey, Priya, Ringo. Revise Holmes, Watson

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,38 @@
 module.exports = {
-  "parserOptions": {
-    "ecmaVersion": 6,
-    "sourceType": "module",
+  'parserOptions': {
+    'ecmaVersion': 6,
+    'sourceType': 'module',
   },
-  "overrides": [{
-    "files": ["test/server.js", "scripts/png.js"],
-    "parserOptions": {
-      "ecmaVersion": 8
+  'overrides': [{
+    'files': ['test/server.js', 'scripts/png.js'],
+    'parserOptions': {
+      'ecmaVersion': 8
     }
   }],
-  "plugins": [
-    "template"          // Handle Tornado templates and JS in HTML files
+  'plugins': [
+    'template'          // Handle Tornado templates and JS in HTML files
   ],
-  "env": {
-    "node": true,       // Include node globals
-    "es6": true,        // Allow ES6 in JavaScript
-    "browser": true,    // Include browser globals
-    "jquery": true      // Include jQuery and $
+  'env': {
+    'node': true,       // Include node globals
+    'es6': true,        // Allow ES6 in JavaScript
+    'browser': true,    // Include browser globals
+    'jquery': true      // Include jQuery and $
   },
-  "globals": {
-    "_": true,
-    "g1": true,
-    "d3": true,
-    "flubber": true,
-    "tape": true
+  'globals': {
+    '_': true,
+    'g1': true,
+    'd3': true,
+    'flubber': true,
+    'tape': true
   },
-  "extends": "eslint:recommended",
-  "rules": {
+  'extends': 'eslint:recommended',
+  'rules': {
     /* Override default rules */
-    "indent": [2, 2, { "VariableDeclarator": 2 }],  // Force 2 space indentation
-    "linebreak-style": ["error", "unix"],           // Force UNIX style line
-    "semi": ["error", "never"],                     // Force no-semicolon style
-    "no-cond-assign": ["off", "always"],            // Allow this for loops
-    "space-before-blocks": ["error", "always"],     // Space must precede brace
-    "quotes": ["error", "single"]                   // We may go for a double-quotes style
+    'indent': [2, 2, { 'VariableDeclarator': 2 }],  // Force 2 space indentation
+    'linebreak-style': ['error', 'unix'],           // Force UNIX style line
+    'semi': ['error', 'never'],                     // Force no-semicolon style
+    'no-cond-assign': ['off', 'always'],            // Allow this for loops
+    'space-before-blocks': ['error', 'always'],     // Space must precede brace
+    'quotes': ['error', 'single']                   // We may go for a double-quotes style
   }
-};
+}
