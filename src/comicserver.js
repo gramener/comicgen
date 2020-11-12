04b76f8c625aa8ded217e330503cf5a330ebfc08
S Anand <root.node@gmail.com>
2020-11-12 15:40:58 +0530

    WIP

--- src/comicserver.js
+++ src/comicserver.js
@@ -1,3 +1,33 @@
-module.exports = function(options) {
-  return ''
+// comicserver() takes a configuration object and returns a fully-rendered SVG
+const fs = require('fs')
+const path = require('path')
+const parser = require('fast-xml-parser')
+
+let comic_root = 'svg/'   // TODO: compute this path
+
+function comic(options) {
+  // comic() or comic({}) return an empty string
+  if (!options || !options.name)
+    return ''
+  let filepath = path.join(comic_root, options.name)
+  // Using the `name=`, find which template to pick up
+  let stat = fs.lstatSync(filepath)
+  let svg
+  // If the template is an SVG file, read it directly
+  if (stat.isFile()) {
+    svg = fs.readFileSync(filepath, 'utf8')
+  }
+  // Else, it’s a folder that should have an `index.svg` and `index.json`
+  else if (stat.isDirectory()) {
+    svg = fs.readFileSync(path.join(filepath, 'index.svg'), 'utf8')
+  }
+  else {
+    throw new Error('TODO')
+  }
+  // If the template contains a <comic> object, recursively replace it with comic()
+  // parser.parse()
+  return svg
 }
+
+
+module.exports = comic
--- test/test-comicserver.js
+++ test/test-comicserver.js
@@ -1,19 +1,47 @@
-var fs = require('fs')
-var test = require('tape')
-var comicgen = require('../src/comicserver')
+const fs = require('fs')
+const test = require('tape')
+const parser = require('fast-xml-parser')
+const comic = require('../src/comicserver')
 
-test('comicgen without options returns an empty string', function (t) {
-  t.equal(comicgen(), '')
+function comicxml(xml) {
+  var config = parser.parse(xml, { ignoreAttributes: false, attributeNamePrefix: "" })
+  return comic(config.comic)
+}
+
+test('comic without options returns an empty string', function (t) {
+  t.equal(comic(), '')
   t.end()
 })
 
-test('comicgen loads a single character', function (t) {
-  let result = comicgen({
-    name: 'dee',
-    angle: 'straight',
-    emotion: 'cry',
-    pose: 'angry'
-  })
-  t.equal(result, fs.readFileSync('test/results/dee-straight-cry-angry.svg'))
+test('comic loads a single SVG file', function (t) {
+  t.equal(
+    comicxml('<comic name="dee/side/emotion/afraid.svg"></comic>'),
+    fs.readFileSync('svg/dee/side/emotion/afraid.svg', 'utf8'))
   t.end()
 })
+
+test('comic parses multiple SVG files', function (t) {
+  t.equal(
+    comicxml([
+      '<comic name="dee/side/emotion/afraid.svg"></comic>',
+      '<comic name="dee/side/pose/angry.svg"></comic>'
+    ].join('')),
+    [
+      fs.readFileSync('svg/dee/side/emotion/afraid.svg', 'utf8'),
+      fs.readFileSync('svg/dee/side/pose/angry.svg', 'utf8')
+    ].join(''))
+  t.end()
+})
+
+
+
+// test('comic loads a single character', function (t) {
+//   let result = comic({
+//     name: 'dee',
+//     angle: 'straight',
+//     emotion: 'cry',
+//     pose: 'angry'
+//   })
+//   t.equal(result, fs.readFileSync('test/results/dee-straight-cry-angry.svg', 'utf8'))
+//   t.end()
+// })
