889fdfe13d8994bc0216fe3335fa98cd89924c55
S Anand <root.node@gmail.com>
2020-12-15 15:18:32 +0530

    Add test cases

--- test/test-comicserver.js
+++ test/test-comicserver.js
@@ -23,7 +23,11 @@ test('comic without options returns an empty string', function (t) {
 test('comic loads a single SVG file', function (t) {
   eq(t,
     comic('<comic name="dee/side/emotion/afraid.svg"></comic>'),
-    fs.readFileSync('svg/dee/side/emotion/afraid.svg', 'utf8'))
+    `<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMin slice" width="500" height="600" viewBox="0 0 500 600">
+      <g transform="translate(250,300) scale(1) translate(-250,-300) translate(0,0)">
+        ${fs.readFileSync('svg/dee/side/emotion/afraid.svg', 'utf8')}
+      </g>
+    </svg>`)
   t.end()
 })
 
@@ -33,10 +37,16 @@ test('comic parses multiple SVG files', function (t) {
       '<comic name="dee/side/emotion/afraid.svg"></comic>',
       '<comic name="dee/side/pose/shrug.svg"></comic>'
     ].join('')),
-    [
-      fs.readFileSync('svg/dee/side/emotion/afraid.svg', 'utf8'),
-      fs.readFileSync('svg/dee/side/pose/shrug.svg', 'utf8')
-    ].join(''))
+    `<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMin slice" width="500" height="600" viewBox="0 0 500 600">
+      <g transform="translate(250,300) scale(1) translate(-250,-300) translate(0,0)">
+        ${fs.readFileSync('svg/dee/side/emotion/afraid.svg', 'utf8')}
+      </g>
+    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMin slice" width="500" height="600" viewBox="0 0 500 600">
+      <g transform="translate(250,300) scale(1) translate(-250,-300) translate(0,0)">
+        ${fs.readFileSync('svg/dee/side/pose/shrug.svg', 'utf8')}
+      </g>
+    </svg>`)
   t.end()
 })
 
@@ -65,32 +75,32 @@ test('comic ignores missing attributes', function (t) {
 })
 
 test('comic works for all characters', function (t) {
-  check(t, 'test/results/aryan-angry-handsfolded.svg', {name: 'aryan', emotion: 'angry', pose: 'handsinpocket' })
+  check(t, 'test/results/aryan-angry-handsfolded.svg', { name: 'aryan', emotion: 'angry', pose: 'handsinpocket' })
   check(t, 'test/results/ava-annoyed-yuhoo.svg', { name: 'ava', emotion: 'annoyed', pose: 'yuhoo' })
-  check(t, 'test/results/bean-straight-curious-handsheldback.svg', {name: 'bean', angle: 'straight', emotion: 'curious', pose: 'handsheldback'})
-  check(t, 'test/results/biden-angry-handsonhip.svg', {name: 'biden', emotion: 'angry', pose: 'handsonhip'})
+  check(t, 'test/results/bean-straight-curious-handsheldback.svg', { name: 'bean', angle: 'straight', emotion: 'curious', pose: 'handsheldback' })
+  check(t, 'test/results/biden-angry-handsonhip.svg', { name: 'biden', emotion: 'angry', pose: 'handsonhip' })
   // check(t, 'test/results/chini-something-something.svg', {name: 'chini', })
-  check(t, 'test/results/dee-straight-angry-handsinpocket.svg', {name: 'dee', angle: 'straight', emotion: 'angry', pose: 'handsinpocket'})
-  check(t, 'test/results/deenuova-straight-angry-handsinpocket.svg', {name: 'deenuova', angle: 'straight', emotion: 'angry', pose: 'handsinpocket'})
+  check(t, 'test/results/dee-straight-angry-handsinpocket.svg', { name: 'dee', angle: 'straight', emotion: 'angry', pose: 'handsinpocket' })
+  check(t, 'test/results/deenuova-straight-angry-handsinpocket.svg', { name: 'deenuova', angle: 'straight', emotion: 'angry', pose: 'handsinpocket' })
   check(t, 'test/results/dey-straight-afraid-angry.svg', { name: 'dey', angle: 'straight', emotion: 'afraid', pose: 'angry' })
-  check(t, 'test/results/deynuovo-straight-afraid-angry.svg', {name: 'deynuovo', angle: 'straight', emotion: 'afraid', pose: 'angry' })
-  check(t, 'test/results/ethan-side-angry-holdingstick.svg', {name: 'ethan', angle: 'side', emotion: 'angry', pose: 'holdingstick'})
-  check(t, 'test/results/evan-angryfrustrated-handsfolded.svg', {name: 'evan', emotion: 'angryfrustrated', pose: 'handsfolded'})
-  check(t, 'test/results/facesketch-face1-ear2-hair1-eyes1-mouth1-nose1.svg', {name: 'facesketch', face: 'face1-ear2', hair: 'hair1', eye: 'eyes1', mouth: 'mouth1', nose: 'nose1'})
+  check(t, 'test/results/deynuovo-straight-afraid-angry.svg', { name: 'deynuovo', angle: 'straight', emotion: 'afraid', pose: 'angry' })
+  check(t, 'test/results/ethan-side-angry-holdingstick.svg', { name: 'ethan', angle: 'side', emotion: 'angry', pose: 'holdingstick' })
+  check(t, 'test/results/evan-angryfrustrated-handsfolded.svg', { name: 'evan', emotion: 'angryfrustrated', pose: 'handsfolded' })
+  check(t, 'test/results/facesketch-face1-ear2-hair1-eyes1-mouth1-nose1.svg', { name: 'facesketch', face: 'face1-ear2', hair: 'hair1', eye: 'eyes1', mouth: 'mouth1', nose: 'nose1' })
   check(t, 'test/results/holmes-straight-cry-shrug.svg', { name: 'holmes', angle: 'straight', emotion: 'cry', pose: 'shrug' })
-  // check(t, 'test/results/humaaans-something-something.svg', {name: 'humaaans', })
-  check(t, 'test/results/jaya-normal-explaining.svg', {name: 'jaya', emotion: 'normal', pose: 'explaining'})
+  check(t, 'test/results/humaaans-front-airy-sitting-skinny-jeans-1-jacket-2.svg', { name: 'humaaans', head: 'front/airy', bottom: 'sitting/skinny-jeans-1', body: 'jacket-2' })
+  check(t, 'test/results/jaya-normal-explaining.svg', { name: 'jaya', emotion: 'normal', pose: 'explaining' })
   // check(t, 'test/results/panda-something-something.svg', {name: 'panda', })
-  check(t, 'test/results/priya-sitting-annoyed-sittingatdesk.svg', {name: 'priya', angle: 'sitting', emotion: 'annoyed', pose: 'sittingatdesk'})
-  check(t, 'test/results/priyanuova-sitting-annoyed-sittingatdesk.svg', {name: 'priyanuova', angle: 'sitting', emotion: 'annoyed', pose: 'sittingatdesk'})
-  check(t, 'test/results/ricky-side-sadlookingup-explaining.svg', {name: 'ricky', angle: 'side', emotion: 'sadlookingup', pose: 'explaining'})
-  check(t, 'test/results/rickynuovo-side-sadlookingup-explaining.svg', {name: 'rickynuovo', angle: 'side', emotion: 'sadlookingup', pose: 'explaining'})
-  check(t, 'test/results/ringo-straight-angry-handsinpocket.svg', {name: 'ringo', angle: 'straight', emotion: 'angry', pose: 'handsinpocket'})
-  check(t, 'test/results/ringonuovo-straight-angry-handsinpocket.svg', {name: 'ringonuovo', angle: 'straight', emotion: 'angry', pose: 'handsinpocket'})
-  check(t, 'test/results/speechbubbles-cloud.svg', {name: 'speechbubbles', speechbubble: 'cloud'})
-  check(t, 'test/results/trump-happy-clapping.svg', {name: 'trump', emotion: 'happy', pose: 'clapping'})
-  check(t, 'test/results/watson-straight-afraid-explaining.svg', {name: 'watson', angle: 'straight', emotion: 'afraid', pose: 'explaining'})
-  check(t, 'test/results/zoe-attitude-holdingbag.svg', {name: 'zoe', emotion: 'attitude', pose: 'holdingbag'})
+  check(t, 'test/results/priya-sitting-annoyed-sittingatdesk.svg', { name: 'priya', angle: 'sitting', emotion: 'annoyed', pose: 'sittingatdesk' })
+  check(t, 'test/results/priyanuova-sitting-annoyed-sittingatdesk.svg', { name: 'priyanuova', angle: 'sitting', emotion: 'annoyed', pose: 'sittingatdesk' })
+  check(t, 'test/results/ricky-side-sadlookingup-explaining.svg', { name: 'ricky', angle: 'side', emotion: 'sadlookingup', pose: 'explaining' })
+  check(t, 'test/results/rickynuovo-side-sadlookingup-explaining.svg', { name: 'rickynuovo', angle: 'side', emotion: 'sadlookingup', pose: 'explaining' })
+  check(t, 'test/results/ringo-straight-angry-handsinpocket.svg', { name: 'ringo', angle: 'straight', emotion: 'angry', pose: 'handsinpocket' })
+  check(t, 'test/results/ringonuovo-straight-angry-handsinpocket.svg', { name: 'ringonuovo', angle: 'straight', emotion: 'angry', pose: 'handsinpocket' })
+  check(t, 'test/results/speechbubbles-cloud.svg', { name: 'speechbubbles', speechbubble: 'cloud' })
+  check(t, 'test/results/trump-happy-clapping.svg', { name: 'trump', emotion: 'happy', pose: 'clapping' })
+  check(t, 'test/results/watson-straight-afraid-explaining.svg', { name: 'watson', angle: 'straight', emotion: 'afraid', pose: 'explaining' })
+  check(t, 'test/results/zoe-attitude-holdingbag.svg', { name: 'zoe', emotion: 'attitude', pose: 'holdingbag' })
   // check(t, 'test/results/zoozoo-something-something.svg', {name: 'zoozoo', })
   t.end()
 })
